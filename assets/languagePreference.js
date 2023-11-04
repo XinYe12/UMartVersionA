eb8ae3240a7ca6d1271996a7f90c18d95f1a2f52
xxu462 <94419600+xxu462@users.noreply.github.com>
2023-11-04 16:33:20 -0400

    testing language window

--- assets/languagePreference.js
+++ assets/languagePreference.js
@@ -1,10 +1,16 @@
-document.getElementById("open-dialog-button").addEventListener("click", function () {
-    document.getElementById("language-dialog").style.display = "block";
-  });
-  
-  document.getElementById("close-dialog-button").addEventListener("click", function () {
-    document.getElementById("language-dialog").style.display = "none";
-    const selectedLanguage = document.getElementById("language-preferences").value;
-    alert("Language preference selected: " + selectedLanguage);
-  });
-  
+// Check if the dialog has been displayed before
+const dialogDisplayed = localStorage.getItem("dialogDisplayed");
+
+if (!dialogDisplayed) {
+  // Show the dialog
+  document.getElementById("language-dialog").style.display = "block";
+
+  // Set a flag to indicate that the dialog has been displayed
+  localStorage.setItem("dialogDisplayed", "true");
+}
+
+document.getElementById("close-dialog-button").addEventListener("click", function () {
+  document.getElementById("language-dialog").style.display = "none";
+  const selectedLanguage = document.getElementById("language-preferences").value;
+  alert("Language preference selected: " + selectedLanguage);
+});
