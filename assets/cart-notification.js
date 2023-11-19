e7b947348c41f7dd31620db6146bc7d7ca278ae9
xxu462 <94419600+xxu462@users.noreply.github.com>
2023-11-19 01:32:56 -0500

    testing animation

--- assets/cart-notification.js
+++ assets/cart-notification.js
@@ -14,41 +14,47 @@ class CartNotification extends HTMLElement {
   }
 
   open() {
-     // Remove any existing checkmark elements
-     this.notification.querySelectorAll('.checkmark-container').forEach((checkmarkContainer) => {
-      checkmarkContainer.remove();
-  });
+    // Remove any existing checkmark elements
+    this.notification.querySelectorAll('.checkmark-container').forEach((checkmarkContainer) => {
+        checkmarkContainer.remove();
+    });
 
-  // Trigger checkmark animation immediately
-  const checkmarkContainer = document.createElement('div');
-  checkmarkContainer.classList.add('checkmark-container');
+    // Trigger checkmark animation immediately
+    const checkmarkContainer = document.createElement('div');
+    checkmarkContainer.classList.add('checkmark-container');
 
-  const checkmark = document.createElement('div');
-  checkmark.classList.add('checkmark');
-  checkmark.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="white" d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/></svg>
-  `;
+    const checkmark = document.createElement('div');
+    checkmark.classList.add('checkmark');
+    checkmark.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="white" d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/></svg>
+    `;
 
-  checkmarkContainer.appendChild(checkmark);
-  this.notification.appendChild(checkmarkContainer);
+    const message = document.createElement('div');
+    message.classList.add('message');
+    message.textContent = 'Product added to cart';
 
-  // Continue with the rest of the open function
-  this.notification.classList.add('animate', 'active');
+    checkmarkContainer.appendChild(checkmark);
+    checkmarkContainer.appendChild(message);
+    this.notification.appendChild(checkmarkContainer);
 
-  // Set focus and trap focus as before
-  this.notification.addEventListener('transitionend', () => {
-      this.notification.focus();
-      trapFocus(this.notification);
-  }, { once: true });
+    // Continue with the rest of the open function
+    this.notification.classList.add('animate', 'active');
 
-  // Set a timeout to remove the checkmark after 5 seconds
-  setTimeout(() => {
-      checkmarkContainer.remove();
-  }, 5000);
+    // Set focus and trap focus as before
+    this.notification.addEventListener('transitionend', () => {
+        this.notification.focus();
+        trapFocus(this.notification);
+    }, { once: true });
 
-  // Add the event listener for body click
-  document.body.addEventListener('click', this.onBodyClick);
-  }
+    // Set a timeout to remove the checkmark after 5 seconds
+    setTimeout(() => {
+        checkmarkContainer.remove();
+    }, 5000);
+
+    // Add the event listener for body click
+    document.body.addEventListener('click', this.onBodyClick);
+}
+
   
   
   
